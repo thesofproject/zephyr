34eaaadbe7a9d6507438d3c78a14d584594d4e1a
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2020-04-22 14:48:31 +0300

    tests: audio/sof: Add tests for alloc API testing
    Add initial tests for allocation API testing. Can be extended for
    other later.

---
+++ tests/subsys/audio/sof/src/main.c
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <ztest.h>
+
+extern void test_audio_sof_alloc(void);
+extern void test_audio_sof_rzalloc(void);
+
+/*test case main entry*/
+void test_main(void)
+{
+	ztest_test_suite(audio_sof,
+			 ztest_unit_test(test_audio_sof_alloc),
+			 ztest_unit_test(test_audio_sof_rzalloc));
+	ztest_run_test_suite(audio_sof);
+}
