3c61327dd9d95a03e6ea1f2c1b2ba3cd1ff650c0
Andy Ross <andrew.j.ross@intel.com>
2020-06-02 15:07:28 -0700

    kernel/printk: Make it synchronous
    Currently printk isn't synchronized except at the byte output level,
    leading to interleaving of messages on SMP systems that try to log
    simultaneously.  This is actually fairly amusing, and actually helpful
    occasionally to validate inter-CPU contention down to the "few cycles"
    level.
    
    Still, when you're printing data you need to read, you need to be able
    to read it.  Put a spinlock around each buffered line.  This has to
    happen in a few places, as there are three different code paths taken
    for !USERSPACE, syscall, and user mode.
    
    Signed-off-by: Andy Ross <andrew.j.ross@intel.com>

--- lib/os/printk.c
+++ lib/os/printk.c
@@ -37,6 +37,8 @@ static void _printk_hex_ulong(out_func_t out, void *ctx,
 			      const unsigned long long num, enum pad_type padding,
 			      int min_width);
 
+static struct k_spinlock lock;
+
 #ifdef CONFIG_PRINTK
 /**
  * @brief Default character output routine that does nothing
@@ -340,26 +342,32 @@ void vprintk(const char *fmt, va_list ap)
 		}
 	} else {
 		struct out_context ctx = { 0 };
+		k_spinlock_key_t key = k_spin_lock(&lock);
 
 		z_vprintk(char_out, &ctx, fmt, ap);
+		k_spin_unlock(&lock, key);
 	}
 }
 #else
 void vprintk(const char *fmt, va_list ap)
 {
 	struct out_context ctx = { 0 };
+	k_spinlock_key_t key = k_spin_lock(&lock);
 
 	z_vprintk(char_out, &ctx, fmt, ap);
+	k_spin_unlock(&lock, key);
 }
 #endif /* CONFIG_USERSPACE */
 
 void z_impl_k_str_out(char *c, size_t n)
 {
 	size_t i;
+	k_spinlock_key_t key = k_spin_lock(&lock);
 
 	for (i = 0; i < n; i++) {
 		_char_out(c[i]);
 	}
+	k_spin_unlock(&lock, key);
 }
 
 #ifdef CONFIG_USERSPACE
