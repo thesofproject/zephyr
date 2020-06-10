c36e69957a49d3bfc2c7cdf39774fa9585a77f9b
Andrei Emeltchenko <andrei.emeltchenko@intel.com>
2020-06-10 17:53:58 +0300

    soc: intel_adsp: Clean up soc.h
    Remove unused or duplicated definitions.
    
    Signed-off-by: Andrei Emeltchenko <andrei.emeltchenko@intel.com>

--- soc/xtensa/intel_adsp/common/include/soc.h
+++ soc/xtensa/intel_adsp/common/include/soc.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019 Intel Corporation
+ *
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -8,9 +9,7 @@
 
 #include <sys/sys_io.h>
 
-/* TODO: need full relative path atm sine soc.h is globally included ?
- * TODO: de-duplicate soc.h in soc and ADSP common headers */
-#include "../common/include/adsp/cache.h"
+#include <adsp/cache.h>
 
 #ifndef __INC_SOC_H
 #define __INC_SOC_H
@@ -49,44 +48,6 @@
 #define IOAPIC_EDGE				0
 #define IOAPIC_HIGH				0
 
-/* low power DMACs */
-#define LP_GP_DMA_SIZE				0x00001000
-#define DW_DMA0_BASE_ADDR			0x0000C000
-#define DW_DMA1_BASE_ADDR			(0x0000C000 +\
-						1 * LP_GP_DMA_SIZE)
-#define DW_DMA2_BASE_ADDR			(0x0000C000 +\
-						2 * LP_GP_DMA_SIZE)
-
-#define DW_DMA0_IRQ				0x00001110
-#define DW_DMA1_IRQ				0x0000010A
-#define DW_DMA2_IRQ				0x0000010D
-
-/* address of DMA ownership register. We need to properly configure
- * this register in order to access the DMA registers.
- */
-#define CAVS_DMA0_OWNERSHIP_REG			(0x00071A60)
-#define CAVS_DMA1_OWNERSHIP_REG			(0x00071A62)
-#define CAVS_DMA2_OWNERSHIP_REG			(0x00071A64)
-
-#define DMA_HANDSHAKE_DMIC_RXA			0
-#define DMA_HANDSHAKE_DMIC_RXB			1
-#define DMA_HANDSHAKE_SSP0_TX			2
-#define DMA_HANDSHAKE_SSP0_RX			3
-#define DMA_HANDSHAKE_SSP1_TX			4
-#define DMA_HANDSHAKE_SSP1_RX			5
-#define DMA_HANDSHAKE_SSP2_TX			6
-#define DMA_HANDSHAKE_SSP2_RX			7
-#define DMA_HANDSHAKE_SSP3_TX			8
-#define DMA_HANDSHAKE_SSP3_RX			9
-
-/* DMA Channel Allocation
- * FIXME: I2S Driver assigns channel in Kconfig.
- * Perhaps DTS is a better option
- */
-#define DMIC_DMA_DEV_NAME			CONFIG_DMA_0_NAME
-#define DMA_CHANNEL_DMIC_RXA			0
-#define DMA_CHANNEL_DMIC_RXB			1
-
 /* I2S */
 #define I2S_CAVS_IRQ(i2s_num)			\
 	SOC_AGGREGATE_IRQ(0, (i2s_num), CAVS_L2_AGG_INT_LEVEL5)
@@ -100,57 +61,7 @@
 #define SSP_MN_DIV_BASE(x)			\
 	(0x00078D00 + ((x) * SSP_MN_DIV_SIZE))
 
-/* MCLK control */
-#define SOC_MCLK_DIV_CTRL_BASE			0x00008E00
-#define SOC_NUM_MCLK_OUTPUTS			2
-#define SOC_MDIVCTRL_MCLK_OUT_EN(mclk)		BIT(mclk)
-#define SOC_MDIVXR_SET_DIVIDER_BYPASS		BIT_MASK(12)
-
-struct soc_mclk_control_regs {
-	uint32_t	mdivctrl;
-	uint32_t	reserved[31];
-	uint32_t	mdivxr[SOC_NUM_MCLK_OUTPUTS];
-};
-
 #define PDM_BASE				0x00010000
-
-#define SOC_NUM_LPGPDMAC			3
-#define SOC_NUM_CHANNELS_IN_DMAC		8
-
-/* SOC Resource Allocation Registers */
-#define SOC_RESOURCE_ALLOC_REG_BASE		0x00071A60
-/* bit field definition for LP GPDMA ownership register */
-#define SOC_LPGPDMAC_OWNER_DSP			\
-	(BIT(15) | BIT_MASK(SOC_NUM_CHANNELS_IN_DMAC))
-
-#define SOC_NUM_I2S_INSTANCES			4
-/* bit field definition for IO peripheral ownership register */
-#define SOC_DSPIOP_I2S_OWNSEL_DSP		\
-	(BIT_MASK(SOC_NUM_I2S_INSTANCES) << 8)
-#define SOC_DSPIOP_DMIC_OWNSEL_DSP		BIT(0)
-
-/* bit field definition for general ownership register */
-#define SOC_GENO_TIMESTAMP_OWNER_DSP		BIT(2)
-#define SOC_GENO_MNDIV_OWNER_DSP		BIT(1)
-
-struct soc_resource_alloc_regs {
-	union {
-		uint16_t	lpgpdmacxo[SOC_NUM_LPGPDMAC];
-		uint16_t	reserved[4];
-	};
-	uint32_t	dspiopo;
-	uint32_t	geno;
-};
-
-/* DMIC SHIM Registers */
-#define SOC_DMIC_SHIM_REG_BASE			0x00071E80
-#define SOC_DMIC_SHIM_DMICLCTL_SPA		BIT(0)
-#define SOC_DMIC_SHIM_DMICLCTL_CPA		BIT(8)
-
-struct soc_dmic_shim_regs {
-	uint32_t	dmiclcap;
-	uint32_t	dmiclctl;
-};
 
 /* SOC DSP SHIM Registers */
 #define SOC_DSP_SHIM_REG_BASE			0x00001000
@@ -245,7 +156,6 @@ struct soc_dsp_shim_regs {
 	uint32_t	lpsdmas1;
 	uint32_t	reserved4[22];
 };
-
 
 extern void z_soc_irq_enable(uint32_t irq);
 extern void z_soc_irq_disable(uint32_t irq);
