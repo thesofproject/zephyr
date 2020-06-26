e0c782e15c93b2f50e93b88d56cbf81ec5a8fb81
Liam Girdwood <liam.r.girdwood@linux.intel.com>
2020-06-26 15:13:00 +0100

    soc: xtensa: intel_adsp: removed unused header macros from memory.h
    Used for SOF only so remove.
    
    Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

--- soc/xtensa/intel_adsp/common/include/cavs/memory.h
+++ soc/xtensa/intel_adsp/common/include/cavs/memory.h
@@ -85,61 +85,6 @@
 #define SRAM_ALIAS_MASK		0xFF000000
 #define SRAM_ALIAS_OFFSET	0x20000000
 
-#if !defined UNIT_TEST && !defined __ZEPHYR__
-#define uncache_to_cache(address) \
-	((__typeof__((address)))((uint32_t)((address)) + SRAM_ALIAS_OFFSET))
-#define cache_to_uncache(address) \
-	((__typeof__((address)))((uint32_t)((address)) - SRAM_ALIAS_OFFSET))
-#define is_uncached(address) \
-	(((uint32_t)(address) & SRAM_ALIAS_MASK) == SRAM_ALIAS_BASE)
-#else
-#define uncache_to_cache(address)	address
-#define cache_to_uncache(address)	address
-#define is_uncached(address)		0
-#endif
-
-/**
- * \brief Returns pointer to the memory shared by multiple cores.
- * \param[in,out] ptr Initial pointer to the allocated memory.
- * \param[in] bytes Size of the allocated memory
- * \return Appropriate pointer to the shared memory.
- *
- * This function is called only once right after allocation of shared memory.
- * Platforms with uncached memory region should return aliased address.
- * On platforms without such region simple invalidate is enough.
- */
-static inline void *platform_shared_get(void *ptr, int bytes)
-{
-#if PLATFORM_CORE_COUNT > 1
-	dcache_invalidate_region(ptr, bytes);
-	return cache_to_uncache(ptr);
-#else
-	return ptr;
-#endif
-}
-
-/**
- * \brief Function for keeping shared data synchronized.
- * It's used after usage of data shared by different cores.
- * Such data is either statically marked with SHARED_DATA
- * or dynamically allocated with SOF_MEM_FLAG_SHARED flag.
- * cAVS platforms use uncached memory region, so no additional
- * synchronization is needed, but for SMP platforms without uncache
- * this macro should writeback and invalidate data.
- */
-static inline void platform_shared_commit(void *ptr, int bytes) { }
-
-/**
- * \brief Transforms pointer if necessary before freeing the memory.
- * \param[in,out] ptr Pointer to the allocated memory.
- * \return Appropriate pointer to the memory ready to be freed.
- */
-static inline void *platform_rfree_prepare(void *ptr)
-{
-	/* free should operate only on cached addresses */
-	return is_uncached(ptr) ? uncache_to_cache(ptr) : ptr;
-}
-
 #endif
 
 /**
@@ -148,7 +93,5 @@ static inline void *platform_rfree_prepare(void *ptr)
 #include <cavs/cpu.h>
 /* SOF Core S configuration */
 #define SRAM_BANK_SIZE		(64 * 1024)
-#define SOF_CORE_S_SIZE		SRAM_BANK_SIZE
-#define SOF_CORE_S_T_SIZE	((PLATFORM_CORE_COUNT - 1) * SOF_CORE_S_SIZE)
 
 #endif /* __CAVS_MEMORY_H__ */
