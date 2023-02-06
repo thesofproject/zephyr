21d1f360d75d0f2ccbe59cc63746633023a3dfa6
Tomasz Leman <tomasz.m.leman@intel.com>
2023-02-06 15:49:50 +0100

    drivers: gpdma: instance power off
    This patch adds function that is allowing to disable power of the DMA
    instance.
    
    Signed-off-by: Tomasz Leman <tomasz.m.leman@intel.com>

--- drivers/dma/dma_intel_adsp_gpdma.c
+++ drivers/dma/dma_intel_adsp_gpdma.c
@@ -234,6 +234,15 @@ static int intel_adsp_gpdma_enable(const struct device *dev)
 
 	return 0;
 }
+
+static int intel_adsp_gpdma_disable(const struct device *dev)
+{
+	const struct intel_adsp_gpdma_cfg *const dev_cfg = dev->config;
+	uint32_t reg = dev_cfg->shim + GPDMA_CTL_OFFSET;
+
+	sys_write32(sys_read32(reg) & ~SHIM_CLKCTL_LPGPDMA_SPA, reg);
+	return 0;
+}
 #endif
 
 static int intel_adsp_gpdma_power_on(const struct device *dev)
@@ -275,6 +284,20 @@ static int intel_adsp_gpdma_power_on(const struct device *dev)
 out:
 	return 0;
 }
+
+#ifdef CONFIG_PM_DEVICE
+static int intel_adsp_gpdma_power_off(const struct device *dev)
+{
+	LOG_INF("%s: dma %s power off", __func__,
+		dev->name);
+#ifdef CONFIG_SOC_SERIES_INTEL_ACE
+	/* Power down */
+	return intel_adsp_gpdma_disable(dev);
+#else
+	return 0;
+#endif /* CONFIG_SOC_SERIES_INTEL_ACE */
+}
+#endif /* CONFIG_PM_DEVICE */
 
 int intel_adsp_gpdma_get_status(const struct device *dev, uint32_t channel, struct dma_status *stat)
 {
@@ -329,6 +352,7 @@ static int gpdma_pm_action(const struct device *dev, enum pm_device_action actio
 	case PM_DEVICE_ACTION_TURN_ON:
 		return intel_adsp_gpdma_power_on(dev);
 	case PM_DEVICE_ACTION_TURN_OFF:
+		return intel_adsp_gpdma_power_off(dev);
 	case PM_DEVICE_ACTION_SUSPEND:
 	case PM_DEVICE_ACTION_RESUME:
 		break;
